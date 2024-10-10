c076ca303f92083b16b04ea4860808fb595c0d3b
rajrfj <rfjfake@gmail.com>
2024-10-10 17:15:02 +0530

    Add files via upload

---
+++ ajson.js
@@ -0,0 +1,35 @@
+
+var url = "http://universities.hipolabs.com/search?name=";
+
+// var btn = document.querySelector("button");
+// btn.addEventListener("click",async function(){
+     
+//     var input = document.querySelector("input").value;
+//     var collArray =  await getCollages(input);
+//     showCollages(collArray)    
+// })
+// function showCollages(collArray)
+// {
+//     var ul = document.querySelector("ul");
+//     ul.innerText = " "
+//     for(collges of collArray)
+//      {
+//         var list = document.createElement("li");
+//         list.innerText = collges.name;
+//         ul.appendChild(list)
+//      }   
+// }
+// async function getCollages(input) {
+//     try{
+//         var result = await axios.get(url+input);
+//          console.log(result.data);
+//          return result.data;    
+//     }catch(error){
+//          console.log("error are found",error);  
+//     }
+// }
+
+
+
+
+
